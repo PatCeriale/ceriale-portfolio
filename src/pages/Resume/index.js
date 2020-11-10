b4e1f4813883941f734b20f609b09a503066407a
Patrick Ceriale <patceriale@gmail.com>
2020-11-09 18:07:08 -0800

    added functionality to download button

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -196,7 +196,9 @@ function Resume() {
                 </p>
               </div>
             </div>
-            <button className="resume-btn">Download to PDF</button>
+            <a href="/images/CerialeResume11:9:20.pdf" download>
+              <button className="resume-btn">Download to PDF</button>
+            </a>
           </div>
         </div>
       </div>
