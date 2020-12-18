6672c75f5a542b5e9842e030567b7f0021a2830d
Patrick Ceriale <patceriale@gmail.com>
2020-12-17 17:38:46 -0800

    fixed footer and bird flying off the page issue

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,25 +4,24 @@ import "./style.css";
 export default function Home() {
   return (
     <div className="Home">
-      {/* <div class="container"> */}
-      <h4>Welcome to Patrick Ceriale's page</h4>
+      <h4>Welcome to my page!</h4>
+      <div class="birdroot">
+        <div class="bird-container bird-container--one">
+          <div class="bird bird--one"></div>
+        </div>
 
-      <div class="bird-container bird-container--one">
-        <div class="bird bird--one"></div>
-      </div>
+        <div class="bird-container bird-container--two">
+          <div class="bird bird--two"></div>
+        </div>
 
-      <div class="bird-container bird-container--two">
-        <div class="bird bird--two"></div>
-      </div>
+        <div class="bird-container bird-container--three">
+          <div class="bird bird--three"></div>
+        </div>
 
-      <div class="bird-container bird-container--three">
-        <div class="bird bird--three"></div>
-      </div>
-
-      <div class="bird-container bird-container--four">
-        <div class="bird bird--four"></div>
+        <div class="bird-container bird-container--four">
+          <div class="bird bird--four"></div>
+        </div>
       </div>
     </div>
-    // </div>
   );
 }
