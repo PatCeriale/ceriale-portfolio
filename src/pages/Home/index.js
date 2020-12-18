b3f4fb7751b068cfeeb4aad930c8c5b7a43e7a55
Patrick Ceriale <patceriale@gmail.com>
2020-12-17 17:13:08 -0800

    got birds working need to fix footer

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,5 +2,27 @@ import React from "react";
 import "./style.css";
 
 export default function Home() {
-  return <div className="Home"></div>;
+  return (
+    <div className="Home">
+      {/* <div class="container"> */}
+      <h4>Welcome to Patrick Ceriale's page</h4>
+
+      <div class="bird-container bird-container--one">
+        <div class="bird bird--one"></div>
+      </div>
+
+      <div class="bird-container bird-container--two">
+        <div class="bird bird--two"></div>
+      </div>
+
+      <div class="bird-container bird-container--three">
+        <div class="bird bird--three"></div>
+      </div>
+
+      <div class="bird-container bird-container--four">
+        <div class="bird bird--four"></div>
+      </div>
+    </div>
+    // </div>
+  );
 }
