8e34c57d70d36bf17eb9d9eedae98068ac98ce17
Patrick Ceriale <patceriale@gmail.com>
2020-12-17 17:22:08 -0800

    fixed the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,21 +8,22 @@ export default function Footer() {
   return (
     <div className="Footer">
       <footer className="card-footer text-muted">
-        {/* <div className="container"> */}{" "}
-        <a
-          href="https://www.linkedin.com/in/patrick-ceriale-2537101b1/"
-          target="blank"
-        >
-          LinkedIn |{" "}
-        </a>
-        <a href="https://github.com/PatCeriale" target="blank">
-          GitHub |{" "}
-        </a>{" "}
-        <a href="mailto: patceriale@gmail.com" target="_blank">
-          Email |
-        </a>
-        <Link to="/resume"> Resume</Link>
-        {/* </div> */}
+        <div className="container">
+          {" "}
+          <a
+            href="https://www.linkedin.com/in/patrick-ceriale-2537101b1/"
+            target="blank"
+          >
+            LinkedIn |{" "}
+          </a>
+          <a href="https://github.com/PatCeriale" target="blank">
+            GitHub |{" "}
+          </a>{" "}
+          <a href="mailto: patceriale@gmail.com" target="_blank">
+            Email |
+          </a>
+          <Link to="/resume"> Resume</Link>
+        </div>
       </footer>
     </div>
   );
